132e4e7a2a90519fa19d80c8a02fd3507f6cd44c
Gustavo Bueno <gustavo.bueno89@gmail.com>
2017-03-25 19:12:56 +0100

    add scss files and boostrap lib

--- src/js/search/search.container.js
+++ src/js/search/search.container.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import SearchBar from './search-bar.component';
+
 class SearchContainer extends React.Component {
 
+    constructor() {
+
+    }
+    
     render() {
-        return <h1>Search</h1>
+        return (
+            <div className="search-container">
+                <h1>Search</h1>
+                <SearchBar />
+            </div>
+        );
     }
 }
 
