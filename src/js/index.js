bb6219017fa0d0e8dc5947f0a62bdd12f8e9ab2e
Gustavo Bueno <gustavo.bueno89@gmail.com>
2017-03-25 18:27:55 +0100

    refactor index.js, add search container

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,38 @@
-'use strict';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, browserHistory, Route, IndexRoute } from 'react-router';
-import { createStore, applyMiddleware } from 'redux';
-import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
+import { createBrowserHistory } from 'history';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+//browser history
+const history = createBrowserHistory();
+
+//middleware
+const middleware = routerMiddleware(history);
+
+//create store
+const store = createStore(
+    combineReducers({
+        reducers,
+        router: routerReducer
+    }),
+    applyMiddleware(middleware, thunk)
+);
+
+import SearchContainer from './search/search.container';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history} >
+            <div>
+                <Route exact={true} path="/" component={SearchContainer}/>
+            </div>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+);
