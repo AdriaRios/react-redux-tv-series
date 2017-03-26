9fb2d7d4f5b0e1f71d14b06be81284feb7146745
Gustavo Bueno <gustavo.bueno89@gmail.com>
2017-03-26 17:31:11 +0200

    add serie image

--- src/js/search/serie-list.component.js
+++ src/js/search/serie-list.component.js
@@ -14,14 +14,24 @@ class SerieList extends React.Component {
         return this.props.series.map((serie) => {
 
             return (
-                <li className="list-group-item" key={serie.id} onClick={ () => {this.showSerieDetails(serie)}}>
+                <li className="list-group-item" key={serie.id} onClick={() => this.showSerieDetails(serie)}>
                     <div className="row">
-                        <div className="col-md-6 text-left">{serie.name}</div>
-                        <div className="col-md-6 text-right">{serie.first_air_date}</div>
+                        <div className="col-md-1 ">
+                            {
+                                serie.poster_path ?
+                                    <img src={`https://image.tmdb.org/t/p/w45_and_h67_bestv2${serie.poster_path}`} alt={serie.name}/> :
+                                    <div className="no-image text-center"><i className="fa fa-picture-o fa-2x" aria-hidden="true"></i></div>
+                            }
+                        </div>
+                        <div className="col-md-11">
+                            <div className="col-md-12 lead">{serie.name}</div>
+                            <div className="col-md-12">{serie.first_air_date}</div>
+                        </div>
+
                     </div>
                 </li>
-            )
-        })
+            );
+        });
     }
 
     render() {
